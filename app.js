32607a2c54d0a6050c0343e6b5aba932c0badb9b
Codmonk <pnksm009@gmail.com>
2021-06-10 16:16:22 +0700

    finish bank and category views

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/myapp', {
+mongoose.connect('mongodb://localhost:27017/db_mernbwa', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -22,7 +25,14 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(methodOverride('_method'));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}));
+app.use(flash());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,21 @@
 const router = require('express').Router();
 const adminController = require('../controllers/adminController');
+const { upload } = require('../middlewares/multer');
 
 router.get('/dashboard', adminController.viewDashboard);
-router.get('/category', adminController.viewCategory);
-router.get('/bank', adminController.viewBank);
 router.get('/item', adminController.viewItem);
 router.get('/booking', adminController.viewBooking);
+
+// Category
+router.get('/category', adminController.viewCategory);
+router.post('/category/add', adminController.addCategory);
+router.put('/category/edit', adminController.updateCategory);
+router.delete('/category/delete/:id', adminController.deleteCategory);
+
+// Bank
+router.get('/bank', adminController.viewBank);
+router.post('/bank/add', upload, adminController.addBank);
+router.put('/bank/edit', upload, adminController.editBank);
+router.delete('/bank/delete/:id', adminController.deleteBank);
 
 module.exports = router;
