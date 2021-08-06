e4395be0af09cd620b39ca86dadc2e12dfee021e
Codmonk <pnksm009@gmail.com>
2021-08-06 09:46:34 +0700

    add cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const cors = require('cors');
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://nott:forlearning@cluster0.d6nox.mongodb.net/db_staycation?retryWrites=true&w=majority', {
@@ -39,6 +40,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 app.use('/sb-admin-2', express.static(path.join(__dirname, 'node_modules/startbootstrap-sb-admin-2')));
 
 app.use('/', indexRouter);
